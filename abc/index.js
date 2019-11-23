f8251fb70ec0a60e940da80f6ec06e4a162f5516
Derek Rodriguez <drod2169@gmail.com>
2019-11-23 09:39:09 -0500

    Add extra error handling

--- abc/index.js
+++ abc/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
-const CLUB_NUMBER = 1 // temp
-const API_ENDPOINT = 'https://api.abcfinancial.com/rest/2/'
+const CLUB_NUMBER = 6951
+const API_ENDPOINT = 'https://api.abcfinancial.com/rest'
 const API_KEY = '5305a1a07f233765bc88e660abc9cb43'
 const API_APP_ID = '671036b1'
 const headers = {
@@ -13,25 +13,32 @@ const getCreatedDateParams = (currDate, prevDate) => ({ createdTimestampRange: `
 const getModifiedDateParams = (currDate, prevDate) => ({ lastModifiedTimestampRange: `${currDate},${prevDate}` })
 const getCheckInParams = (currDate, prevDate) => ({ lastCheckInTimestamp: `${currDate},${prevDate}` })
 
-function response(res) {
+function shapeResponse(res) {
   if (res.status.message !== 'success') throw new Error('Error fetching members')
 
   return res.members
 }
 
+function handleError(error) {
+  console.error('Error occurred::', error)
+}
+
 function getMembersCheckIns(currDate, prevDate) {
   return axios.get(`${API_ENDPOINT}/${CLUB_NUMBER}/members`, { headers, params: getCheckInParams(currDate, prevDate) })
-    .then(response)
+    .then(shapeResponse)
+    .catch(handleError)
 }
 
 function getMembersCreated(currDate, prevDate) {
   return axios.get(`${API_ENDPOINT}/${CLUB_NUMBER}/members`, { headers, params: getCreatedDateParams(currDate, prevDate) })
-    .then(response)
+    .then(shapeResponse)
+    .catch(handleError)
 }
 
 function getMembersModified(currDate, prevDate) {
   return axios.get(`${API_ENDPOINT}/${CLUB_NUMBER}/members`, { headers, params: getModifiedDateParams(currDate, prevDate) })
-    .then(response)
+    .then(shapeResponse)
+    .catch(handleError)
 }
 
 module.exports = { getMembersCheckIns, getMembersCreated, getMembersModified }

749c791bc9ba95019a473478a4645954ace1be9e
Derek Rodriguez <drod2169@gmail.com>
2019-11-23 09:35:35 -0500

    Add onError functin

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const { Member } = require('./Member/member')
 // const { } = require('./active-campaign')
 
 const updateMembers = (members) => members.map((member) => new Member(member.personal).toJson())
+const onError = (error) => console.error('Error occurred::', error)
 const dateFormat = 'YYYY-MM-DD HH:mm:ss.SSSSSS'
 
 async function main() {
   const prevDate = moment(getPreviousDate(), dateFormat).format(dateFormat)
   const currDate = moment(new Date()).format(dateFormat)
   
-  const membersCreated = await getMembersCreated(currDate, prevDate).then(updateMembers)
-  const membersModified = await getMembersModified(currDate, prevDate).then(updateMembers)
-  const membersCheckins = await getMembersCheckIns(currDate, prevDate).then(updateMembers)
+  const membersCreated = await getMembersCreated(currDate, prevDate).then(updateMembers).catch(onError)
+  const membersModified = await getMembersModified(currDate, prevDate).then(updateMembers).catch(onError)
+  const membersCheckins = await getMembersCheckIns(currDate, prevDate).then(updateMembers).catch(onError)
 
   // After everything is done, update the date
   writeDate(currDate)
